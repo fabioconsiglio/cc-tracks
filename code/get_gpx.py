8707a2a706039f0d318cf1f2d893e2d894004d4f
fabioconsiglio <fabian.mahner@uni-konstanz.de>
2025-05-05 16:48:02 +0200

    toskana-tracks

---
+++ code/get_gpx.py
@@ -0,0 +1,22 @@
+## get gpx via strava api 
+
+
+from __future__ import print_statement
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+
+# Configure OAuth2 access token for authorization: strava_oauth
+swagger_client.configuration.access_token = 'YOUR_ACCESS_TOKEN'
+
+# create an instance of the API class
+api_instance = swagger_client.RoutesApi()
+id = 14107453502 # Long | The identifier of the route.
+
+try: 
+    # Export Route GPX
+    api_instance.getRouteAsGPX(id)
+except ApiException as e:
+    print("Exception when calling RoutesApi->getRouteAsGPX: %s\n" % e)
+
